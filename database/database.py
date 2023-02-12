89275547da6272052c7c56b208e818df7af4efce
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2023-02-12 15:54:43 +0300

    Add date to message about matches

--- database/database.py
+++ database/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_NAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_USER, SQLITE_URL
+from config import DATABASE_NAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_USER, SQLITE_URL, DATABASE_PORT
 
 
-# engine = create_engine(f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5433/{DATABASE_NAME}')
-engine = create_engine(f'{SQLITE_URL}', connect_args={"check_same_thread": False})
+engine = create_engine(f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}{DATABASE_PORT}/{DATABASE_NAME}')
+# engine = create_engine(f'{SQLITE_URL}', connect_args={"check_same_thread": False})
 Session = sessionmaker(
     engine,
     autocommit=False,
